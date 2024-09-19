eedc6eae6e17df8620dda2d08e396002c554a546
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-09-19 13:32:24 +0100

    Update Jar_analysis.sh

--- Non-Equilibrium-Work/Jar_analysis.sh
+++ Non-Equilibrium-Work/Jar_analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
- ./Jar_v5.02 $@ #3 30 1501 1-Series1/ 2-Series2/ 3-Series3/
+ ./Jar_v5.02 $@ #Executalbe num_folders num_sims_per_folder(subfolders) num_data_per_sim folder_1/ folder_2/ .... folder_n/
  ./PlotJar.py
  ./WorkDistribution-v02.py $1 $2 $3
