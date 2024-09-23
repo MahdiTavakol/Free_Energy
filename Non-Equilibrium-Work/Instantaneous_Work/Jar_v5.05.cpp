a0121180b338698ea037f4f6ba5288a072dafe48
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-09-23 15:59:10 +0100

    Update Jar_v5.05.cpp

--- Non-Equilibrium-Work/Instantaneous_Work/Jar_v5.05.cpp
+++ Non-Equilibrium-Work/Instantaneous_Work/Jar_v5.05.cpp
@@ -13,24 +13,24 @@ string version = "5.05";
 int main(int argc, char** argv)
 {
 	cout << "Usage: " << endl;
-	cout << "./Jar_v" << version <<  " numFolders numSims numDataPerSim folder1 folder2 ...." << endl;
+	cout << "./Jar_v" << version << " numFolders numSims numDataPerSim folder1 folder2 ...." << endl;
 	string logName = "Jar_v" + version + ".log";
 	string command = "";
 	string space = " ";
 	for (int i = 0; i < argc; i++) {
-		char * str = argv[i];
-		command +=  space + str;
-		}
-	writelog(logName,command);
-	int numFolders     = stoi(argv[1]);
-	int numSims        = stoi(argv[2]);
-	int numData        = stoi(argv[3]);
-	
-	
-	string * folders = new string[numFolders];
+		char* str = argv[i];
+		command += space + str;
+	}
+	writelog(logName, command);
+	int numFolders = stoi(argv[1]);
+	int numSims = stoi(argv[2]);
+	int numData = stoi(argv[3]);
+
+
+	string* folders = new string[numFolders];
 	for (int i = 0; i < numFolders; i++)
 	{
-		folders[i] = argv[3+1+i];
+		folders[i] = argv[3 + 1 + i];
 	}
 
 	int numDirs = 2; // Number of directions forward and reverse
@@ -44,8 +44,8 @@ int main(int argc, char** argv)
 
 	/* ----------------------------------------------------------------
 
-       ---------------------------------------------------------------- */
-	
+	   ---------------------------------------------------------------- */
+
 	string z1file = "z1s-Ins.txt";
 	string z2file = "z2s-Ins.txt";
 	string z3file = "z3s-Ins.txt";
@@ -59,7 +59,7 @@ int main(int argc, char** argv)
 	string wname = "w";
 
 	string outputFile = "FreeEnergy-Ins.dat";
-	
+
 	string exceptionsFileName = "Exceptions-Ins.txt";
 
 	/* ----------------------------------------------------------------
@@ -67,11 +67,11 @@ int main(int argc, char** argv)
 	   ---------------------------------------------------------------- */
 	cout << "Initializing the arrays" << endl;
 	int* step, * exceptions;
-	double** z1, ** z2, ** z3, ** f, ** wIns, ** w, *gBarIns, *gBar, * gSecIns, * gSec, *wInsAvg, * wAvg, * z1avg;
+	double** z1, ** z2, ** z3, ** f, ** wIns, ** w, * gBarIns, * gBar, * gSecIns, * gSec, * wInsAvg, * wAvg, * z1avg;
 	long double* gExpIns, * gExp;
 
 
-	zeros(step, numDirs*numData); // forward-reverse
+	zeros(step, numDirs * numData); // forward-reverse
 	zeros(z1, numDirs * numData, numSims * numFolders); // forward-reverse + two folders
 	zeros(z2, numDirs * numData, numSims * numFolders); // forward-reverse + two folders
 	zeros(z3, numDirs * numData, numSims * numFolders); // forward-reverse + two folders
@@ -84,24 +84,24 @@ int main(int argc, char** argv)
 
 
 	zeros(w, numDirs * numData, numSims * numFolders); // forward-reverse + two folders
-	zeros(gExp,numData * numDirs); // forward-reverse
-	zeros(gBar,numData * numDirs); // forward-reverse
-	zeros(gSec,numData * numDirs); // forward-reverse
+	zeros(gExp, numData * numDirs); // forward-reverse
+	zeros(gBar, numData * numDirs); // forward-reverse
+	zeros(gSec, numData * numDirs); // forward-reverse
 
 
 	zeros(wInsAvg, numData * numDirs);
-	zeros(wAvg,numData * numDirs); // forward-reverse
+	zeros(wAvg, numData * numDirs); // forward-reverse
 
 
 	zeros(z1avg, numData * numDirs); // forward-reverse
-	zeros(exceptions,numFolders*numSims);
+	zeros(exceptions, numFolders * numSims);
 
 	cout << "Finished initializing the arrays" << endl;
 	/* ----------------------------------------------------------------
 
 	   ---------------------------------------------------------------- */
 	cout << "Reading the simulation data" << endl;
-	readSims(df, folders, exceptions ,step, z1, z2, z3, f);
+	readSims(df, folders, exceptions, step, z1, z2, z3, f);
 	/* ----------------------------------------------------------------
 
 	   ---------------------------------------------------------------- */
@@ -109,7 +109,7 @@ int main(int argc, char** argv)
 	writeData(df, step, z1, z1file, z1name, exceptions);
 	writeData(df, step, z2, z2file, z2name, exceptions);
 	writeData(df, step, z3, z3file, z3name, exceptions);
-	writeData(df, step, f,  ffile,  fname, exceptions);
+	writeData(df, step, f, ffile, fname, exceptions);
 	/* ----------------------------------------------------------------
 
 	   ---------------------------------------------------------------- */
@@ -123,13 +123,13 @@ int main(int argc, char** argv)
 	cout << "Cumulating the work" << endl;
 	cumulateWork(df, wIns, w);
 	cout << "Writing the work" << endl;
-	writeData(df, step, w,  wfile,  fname,  exceptions);
+	writeData(df, step, w, wfile, fname, exceptions);
 	/* ----------------------------------------------------------------
 
 	   ---------------------------------------------------------------- */
 	cout << "Calculating the instantaneous free energy" << endl;
 	calcJar(df, wIns, exceptions, gExpIns, gSecIns, wInsAvg);
-	calcBar(df,gBarIns);
+	calcBar(df, gBarIns);
 	/* ----------------------------------------------------------------
 
 	   ---------------------------------------------------------------- */
@@ -137,13 +137,14 @@ int main(int argc, char** argv)
 	cumulateWork(df, gExpIns, gExp);
 	cumulateWork(df, gSecIns, gSec);
 	cumulateWork(df, gBarIns, gBar);
- 	/* ----------------------------------------------------------------
-	
+	cumulateWork(df, wInsAvg, wAvg);
+	/* ----------------------------------------------------------------
+
 	   ---------------------------------------------------------------- */
 	cout << "Shifting the free energy" << endl;
 	shiftJar(df, gExp, gSec, gBar, wAvg);
 	/* ----------------------------------------------------------------
-	
+
 	   ---------------------------------------------------------------- */
 	cout << "Writing the free energy" << endl;
 	writeOutput(df, outputFile, step, z1avg, gExp, gSec, gBar, wAvg);
@@ -182,4 +183,3 @@ int main(int argc, char** argv)
 	cout << "All done!" << endl;
 	return 0;
 }
-
