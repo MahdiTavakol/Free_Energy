1cb07249f0da38a1c9c7a344e992b2241ed3b010
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-05-20 15:29:17 +0100

    Create Jar_v5.033.cpp

---
+++ Non-Equilibrium-Work/Cumulated_Work/Jar_v5.033.cpp
@@ -0,0 +1,11 @@
+#include "Jr_v033.h"
+#include <memory>
+
+int main(int argc, char** argv)
+{
+	std::unique_ptr<JarCalculator> jc = std::make_unique<JarCalculator>(argc,argv);
+	jc->initialize();
+	jc->calculate();
+	jc->write();
+	return 0;
+}
